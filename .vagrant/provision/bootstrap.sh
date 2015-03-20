f34288c137fafa6c91ce38791beccdb8af49a852
seblavenant <sebastien@lavenant.me>
2015-03-20 22:26:54 +0100

    chore: move files moved badly

--- .vagrant/provision/bootstrap.sh
+++ .vagrant/provision/bootstrap.sh
@@ -8,15 +8,20 @@ if ! [ -L /var/www ]; then
 fi
 
 apt-get update
+
+# INSTALL APACHE
 apt-get install -y apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert
 cp /vagrant/.vagrant/provision/apache2/virtualhost.conf /etc/apache2/sites-enabled/
-
+# INSTALL PHP
 apt-get install -y libapache2-mod-php5 php5 php5-common php5-curl php5-dev php5-gd php5-idn php-pear php5-imagick php5-imap php5-json php5-mcrypt php5-memcache php5-mhash php5-ming php5-mysql php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl
+# INSTALL GIT
+apt-get install -y git
 
-service apache2 restart
-
+# CONFIG USER
 cp /vagrant/.vagrant/provision/home/.bashrc /home/vagrant/
 cp /vagrant/.vagrant/provision/home/.gitconfig /home/vagrant/
 cp /vagrant/.vagrant/provision/home/.vimrc /home/vagrant/
-
 source /home/vagrant/.bashrc
+
+# SERVICE LAUNCH
+service apache2 restart
