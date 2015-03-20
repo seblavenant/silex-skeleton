5fb8438d6593f00cc7445e48f258bf307f4d8dc9
seblavenant <sebastien@lavenant.me>
2015-03-20 22:56:43 +0100

    chore: bootstrap - add application start command

--- .vagrant/provision/bootstrap.sh
+++ .vagrant/provision/bootstrap.sh
@@ -13,6 +13,7 @@ apt-get update
 apt-get install -y apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert
 cp /vagrant/.vagrant/provision/apache2/virtualhost.conf /etc/apache2/sites-enabled/
 apt-get install -y libapache2-mod-php5 php5 php5-common php5-curl php5-dev php5-gd php5-idn php-pear php5-imagick php5-imap php5-json php5-mcrypt php5-memcache php5-mhash php5-ming php5-mysql php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl
+
 # INSTALL OTHER SOFTWARE
 apt-get install -y git
 apt-get install -y vim
@@ -25,3 +26,9 @@ source /home/vagrant/.bashrc
 
 # SERVICE LAUNCH
 service apache2 restart
+
+# APPLICATION
+cd /var/www
+php composer.phar install
+vendor/bin/karma hydrate
+cd -
